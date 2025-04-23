de7a3f19b1951cc1dac452cc28e6dd8ab3dbb446
lkoiescg2031 <lkoiescg2031@naver.com>
2025-04-23 23:53:32 +0900

    커스텀 음악 사전 추가

---
+++ src/entities/music/config/custom-music-dic.ts
@@ -0,0 +1,64 @@
+import { MusicDic } from "@/entities/music/models/music";
+
+export const MUSIC_DICTIONARY: MusicDic = {
+	YBvcVTZefJM: {
+		artist: "아이유(IU)",
+		title: "ベテルギウス",
+	},
+	"6yOgmQY7SEU": {
+		artist: "Homme(창민·이현)",
+		title: "밥만 잘 먹더라",
+	},
+	Z83Oz5vFQns: {
+		artist: "유회승",
+		title: "헤어지는 중입니다",
+	},
+	_HgKsS7hLU0: {
+		artist: "정동하",
+		title: "천년의 사랑",
+	},
+	lkNgJltBFTQ: {
+		artist: "FTISLAND(이홍기)·유회승",
+		title: "사랑했었다 (Still love you)",
+	},
+	"47uKZkE04g8": {
+		artist: "박은태",
+		title: "지저스 크라이스트 수퍼스타 - 겟세마네",
+	},
+	yFRWj2m9A0c: {
+		artist: "윤형렬",
+		title: "지저스 크라이스트 수퍼스타 - Heaven on Their Minds",
+	},
+	bZ3AzBTEpbc: {
+		artist: "소향",
+		title: "Sweet Dreams, My Dear (Korean Version)",
+	},
+	i1dVOHdZhP4: {
+		artist: "V.O.S(최현준)",
+		title: "VOS 미친 것처럼 (현준 solo ver.)",
+	},
+	"tkjaS-0OZaQ": {
+		artist: "로이킴·정준영",
+		title: "먼지가 되어",
+	},
+	"2R5NJHJrjKU": {
+		artist: "Sam Ryder",
+		title: "Tiny Riot",
+	},
+	"-09V6ImAJSY": {
+		artist: "김경현,SiHa",
+		title: "지켜내지 못한 (Don't Cry Part..2)",
+	},
+	"Dll-cWhSJOY": {
+		artist: "Queen (Freddie Mercury)",
+		title: "내 손을 잡아",
+	},
+	BlnSYpgr_yk: {
+		artist: "이성우·서문탁",
+		title: "못다핀 꽃 한송이",
+	},
+	tI07ctFTxg4: {
+		artist: "FTISLAND(이홍기)",
+		title: "질풍가도",
+	},
+};
